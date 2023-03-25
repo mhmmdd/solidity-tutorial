48696ab4f83a061e0382e312d0a30bde65fd3890
mhmmdd <mhmmd.dd@gmail.com>
2023-03-25 08:23:45 +0300

    12-bank-send-ether.sol

---
+++ 12-bank-send-ether.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.8.0;
+
+contract Bank {
+
+    mapping(address => uint) balance;
+
+    // Deposit (pull) ether to the contract
+    function sendEtherToContract() public payable {
+        balance[msg.sender] += msg.value;
+    }
+
+    function showBalance() public view returns (uint) {
+        return balance[msg.sender];
+    }
+
+    // Send ether to the contract sender
+    function withdraw() public {
+        uint amount = balance[msg.sender];
+        balance[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+
+    // Send ether to a specific address
+    function withdraw(address payable to, uint amount) public {
+        balance[msg.sender] -= amount;
+        // transfer reverts the transaction if the amount is greater than the balance
+        to.transfer(amount);
+    }
+
+    // Contract
+    // Send ether to a specific address
+    function withdraw(address payable to, uint amount, bytes memory data) public {
+        balance[msg.sender] -= amount;
+        // transfer reverts the transaction if the amount is greater than the balance
+        bool success = to.send(amount);
+        require(success, "Transfer failed.");
+    }
+
+    function withdraw(address payable to, uint amount, bytes memory data, uint gas) public {
+        balance[msg.sender] -= amount;
+        // transfer reverts the transaction if the amount is greater than the balance
+        (bool success,) = to.call{value: amount, gas: gas}(data);
+        require(success, "Transfer failed.");
+    }
+
+
+    // Receive ether
+    receive() external payable {
+        balance[msg.sender] += msg.value;
+    }
+
+    // Receive ether with data
+    fallback() external payable {
+        balance[msg.sender] += msg.value;
+        data = msg.data; // data should be hex encoded
+    }
+
+}
+
--- README.md
+++ README.md
@@ -14,6 +14,30 @@ Run the test suite with `npm test`.
 ## Changes
 1. 11-struct-enum.sol - added a struct and an enum.
 2. 11-struct-enum.sol - added modifier.<br>
-   **modifier**: it is used to check the condition before executing the function.
+**modifier**: it is used to check the condition before executing the function.
 3. 11-struc-enum.sol - added event.<br>
-   **event**: it is used to log the event in the blockchain.
+**event**: it is used to log the event in the blockchain.
+4. 12-bank-send-ether.sol - send ether to the contract.<br>
+   1. **transfer**: it is used to send ether and throw an error.
+   ```solidity
+    transfer(); // revert if failed
+   ```
+   2. **send**: it is used send ether and return a boolean value. 
+   ```solidity
+    bool success = send();
+   ```
+   3. **call**: it is used to send ether with data.
+   ```solidity
+    (bool success,) = to.call{value: amount, gas: gas}(data);
+   ```
+4. 12-bank-send-ether.sol - receive ether from the contract.<br>
+   1. **receive**: it is used to receive ether.
+   ```solidity
+    receive() external payable {}
+   ```
+   2. **fallback**: it is used to receive ether with data. 
+   ```solidity
+    fallback() external payable {}
+   ```
+   Note: Falback is called when the receive function is not defined or when the data is sent with the transaction.
+5. 
